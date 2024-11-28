625d07af430cba4b55db2abe79512ce85578b6c2
Nguyễn Thị Thùy <nguyenthuy16112020@gmail.com>
2024-11-28 21:10:12 +0700

    first commit

---
+++ BaiTap/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace BaiTap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtFirstname.Location = new System.Drawing.Point(499, 175);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(264, 20);
+            this.txtFirstname.TabIndex = 1;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtLastname.Location = new System.Drawing.Point(499, 116);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(264, 20);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtPhone.Location = new System.Drawing.Point(499, 232);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(264, 20);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btXoa.Location = new System.Drawing.Point(371, 371);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(78, 30);
+            this.btXoa.TabIndex = 2;
+            this.btXoa.Text = "Xoa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btSua
+            // 
+            this.btSua.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSua.Location = new System.Drawing.Point(499, 371);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(78, 30);
+            this.btSua.TabIndex = 2;
+            this.btSua.Text = "Sua";
+            this.btSua.UseVisualStyleBackColor = true;
+            this.btSua.Click += new System.EventHandler(this.btSua_Click);
+            // 
+            // btThem
+            // 
+            this.btThem.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThem.Location = new System.Drawing.Point(245, 371);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(78, 30);
+            this.btThem.TabIndex = 2;
+            this.btThem.Text = "Them";
+            this.btThem.UseVisualStyleBackColor = true;
+            this.btThem.Click += new System.EventHandler(this.btThem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(414, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FirstName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(414, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LastName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(414, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phone";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.LastName,
+            this.FirstName,
+            this.Phone});
+            this.listView1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(77, 105);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(298, 166);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+ 
+            // 
+            // LastName
+            // 
+            this.LastName.Text = "LastName";
+            this.LastName.Width = 100;
+            // 
+            // FirstName
+            // 
+            this.FirstName.Text = "FirstName";
+            this.FirstName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.FirstName.Width = 100;
+            // 
+            // Phone
+            // 
+            this.Phone.Text = "Phone";
+            this.Phone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Phone.Width = 100;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-1, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(814, 49);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Demo ListView";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btThem);
+            this.Controls.Add(this.btSua);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtFirstname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader LastName;
+        private System.Windows.Forms.ColumnHeader FirstName;
+        private System.Windows.Forms.ColumnHeader Phone;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BaiTap/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BaiTap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Kiểm tra nếu có dòng nào được chọn
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Lấy dòng đang được chọn
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                // Hiển thị dữ liệu dòng lên TextBox
+                txtLastname.Text = selectedItem.Text;              // Cột đầu tiên
+                txtFirstname.Text = selectedItem.SubItems[1].Text; // Cột thứ hai
+                txtPhone.Text = selectedItem.SubItems[2].Text;     // Cột thứ ba
+            }
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            //1.Tạo 1 dòng dữ liệu 
+            ListViewItem lv = new ListViewItem(txtLastname.Text);
+            // thêm dữ liệu vào các cột còn lại 
+            lv.SubItems.Add(txtFirstname.Text);
+            lv.SubItems.Add(txtPhone.Text);
+            // đưa dữ liệu lên listview 
+            listView1.Items.Add(lv);  // Thêm dòng mới vào ListView
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtFirstname.Text == "" || txtPhone.Text == "" || txtLastname.Text == "")
+                {
+                    throw new Exception("Vui long nhap day du thong tin sinh vien!");
+                }
+                if (listView1.SelectedItems.Count > 0) // Giả sử sử dụng DataGridView
+                {
+                    int selectedRow = listView1.SelectedItems[0].Index; // Lấy chỉ số dòng được chọn
+                    listView1.Items.RemoveAt(selectedRow);            
+                    MessageBox.Show("Đã xóa thành công!", "Tiêu đề thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                }
+                else
+                {
+                    throw new Exception("Vui lòng chọn một sinh viên để xóa!");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra nếu có dòng nào được chọn
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Lấy dòng đang được chọn
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                // Cập nhật dữ liệu từ TextBox vào ListView
+                selectedItem.Text = txtLastname.Text;              
+                selectedItem.SubItems[1].Text = txtFirstname.Text; 
+                selectedItem.SubItems[2].Text = txtPhone.Text;    
+
+                MessageBox.Show("Dữ liệu đã được cập nhật thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Không có dòng nào được chọn để sửa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
